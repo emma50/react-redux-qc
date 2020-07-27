f9aed761669dc429e85a942352433a7662ea5ed7
Okwuidegbe Emmanuel <you@example.com>
2020-07-27 22:22:07 +0100

    Refractor code - Modified auth and loan actions - Modified app router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';import { Router } from 'react-router-dom'
 import { Provider } from "react-redux";
 import 'react-toastify/dist/ReactToastify.css'
 import { ThemeProvider } from '@material-ui/core/styles';
+import * as serviceWorker from './serviceWorker';
 import theme from './components/theme/theme';
 import configStore from "./store/configStore";
+import history from './helpers/history'
 import App from './routers/AppRouter';
 
 const store = configStore();
@@ -14,7 +15,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+      <Router history={history}>
       <App/>
+      </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
